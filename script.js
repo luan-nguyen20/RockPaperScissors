2c422295b7149e4d14c3167354c6d0fdbaa06097
Luan <nguyenbaoluan1989@gmail.com>
2020-01-09 12:27:17 -0600

    add comPlay and userPlay funcs

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let randInt;
+let randInt, comChoice, userChoice;
 
 // randomly return Rock, Paper or Scissors
 function comPlay(){
     // assign randomly 0,1 or 2
-    randInt = Math.floor(Math.random()*2);
-    
+    randInt = Math.floor(Math.random()*3);
+
     switch(randInt){
         case 0:
             return('Rock');
@@ -19,7 +19,34 @@ function comPlay(){
     }
 }
 
+//get user selection
+function userPlay(userInput){
+    switch(String(userInput).toUpperCase()){
+        case 'ROCK':
+            return('Rock');
+            break;
+        case 'PAPER':
+            return('Paper');
+            break;
+        case 'SCISSORS':
+            return('Scissors');
+            break;
+        default:
+            alert("Please enter \'Rock\', \'Paper\' or \'Scissors\'");
+            return('Invalid');
+            break;
+    }
+}
+
 //play a single round
 function playRound(playerSelection, comSelection){
+    comChoice = comPlay();
+    userChoice = userPlay(prompt("Please enter \'Rock\', \'Paper\' or \'Scissors\'"));
+    console.log(comChoice + '  ' + userChoice);
+//return round winner
+}
 
+//play multiple rounds and keep scores
+function game(){
+//return game winner
 }
